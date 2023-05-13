1e360846bc29b97d218e769eae043ff25d9a63ca
Artem_Tsetkhalin <artem.tsetkhalin@canarysystems.com>
2023-05-14 00:11:12 +0500

    Change syntax

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ const {token} = settings;
 
 const bot = new TelegramAPI(token, {polling: true});
 
-const caloriesRegEx = /\/calories\s"([А-Яа-я]+)"\s(\d+)/;
+const caloriesRegEx = /"([А-Яа-я]+)"\s(\d+)/;
 const start = () => {
     bot.on("message", async msg => {
         switch (msg.text) {
             case "/start": {
                 const user_name = `${msg.chat.first_name ? msg.chat.first_name : ""} ${msg.chat.last_name ? msg.chat.last_name : ""}`;
                 await bot.sendMessage(msg.chat.id, `Здравствуйте, ${user_name}\n` +
-                    `Введите команду /calories "название_продукта" вес_в_граммах`
+                    `Введите "название_продукта" вес_в_граммах`
                 );
                 break;
             }
